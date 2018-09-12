d3fc557260e2e578e3e32c6c255caeff1f270cc7
trakieca <emil_vaklinov@yahoo.com>
2018-09-12 15:28:34 +0100

    MVP and Extention done

--- src/views/country_info_view.js
+++ src/views/country_info_view.js
@@ -17,11 +17,39 @@ CountryInfoView.prototype.render = function (country){
   const countryName =this.createTextElement('h2', country.name);
   this.container.appendChild(countryName);
 
-  const countryHeader = this.createTextElement('h3', 'Region:');
-  this.container.appendChild(countryHeader);
+  const countryFlag = document.createElement('img');
+  countryFlag.src = country.flag;
+  this.container.appendChild(countryFlag);
+
+  const countryNameHeader = this.createTextElement('h3', 'Region:');
+  this.container.appendChild(countryNameHeader);
 
   const countryRegion = this.createTextElement('p', country.region);
   this.container.appendChild(countryRegion);
+
+  const countryLanguagesHeader = this.createTextElement('h3', 'Languages:');
+  this.container.appendChild(countryLanguagesHeader);
+
+  const countryLanguages = country.languages.forEach((language) => {
+    const countryLanguage = this.createTextElement('p', language.name);
+    this.container.appendChild(countryLanguage);
+  })
+
+  const countryCurrenyHeader = this.createTextElement('h3', 'Currencies:');
+  this.container.appendChild(countryCurrenyHeader);
+
+  const countryCurrencies = country.currencies.forEach((currency) => {
+    const countryCurrency = this.createTextElement('p', currency.name);
+    this.container.appendChild(countryCurrency);
+  })
+
+  const countryTranslationsHeader = this.createTextElement('h3', 'Translations:');
+  this.container.appendChild(countryTranslationsHeader);
+
+  for (translation in country.translations) {
+    const translationParagraph = this.createTextElement('p', translation + ': ' + country.translations[translation]);
+      this.container.appendChild(translationParagraph);
+  }
 
 }
 
